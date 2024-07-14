16ac7b0d7e3f164a89a9bb0739cdbb6d9cf2f279
AllenHeartcore <ziyuanc3@illinois.edu>
2024-07-14 09:03:22 +0800

    [Refactor] Remove deprecated eDiffMode and eWorkingMode support

--- Main.py
+++ Main.py
@@ -1,8 +1,6 @@
 from ManifestDecryptor import doDecrypt
 from MaskedHeaderStream import unObfuscate, rename
 from Downloader import download
-from eDiffMode import DiffMode
-from eWorkingMode import WorkingMode
 
 global output_dir
 output_dir = './gkmas'
@@ -10,7 +8,6 @@ output_dir = './gkmas'
 download_asset = 1
 download_resource = 0
 
-#diffMode = DiffMode.Diff
 jDict = doDecrypt()
 download(jDict, output_dir, download_asset, download_resource)
 unObfuscate(jDict)
