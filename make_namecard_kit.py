548b8f21e7c3ed2594e1217a4acf48d5d27246e2
Ziyuan Chen <ziyuan.chen@columbia.edu>
2024-09-15 03:32:12 -0400

    [Style] In make_namecard_kit, switch to r-string for all patterns in download instructions
    Also ignore .*-highscore-thumb

--- make_namecard_kit.py
+++ make_namecard_kit.py
@@ -11,27 +11,27 @@ from gkmasToolkit.utils import Logger
 # - frame?
 # - misc…
 instructions_dl = [
-    ("img_general_icon_contest-rank.*", "profile", None),
-    ("img_general_meishi_illust_idol.*", "idol/full", None),
-    ("img_general_meishi_illust_sd.*", "idol/mini", "2:3"),
-    ("img_general_cidol.*full\..*", "idol/produce", "9:16"),
-    ("img_general_cidol.*thumb-landscape-large.*", "idol/produce", "16:9"),
-    ("img_general_cidol.*thumb-portrait.*", "idol/produce", "3:4"),
-    ("img_general_meishi_illust_sign.*", "idol/sign", None),
-    ("img_general_csprt.*full\..*", "support", "16:9"),
-    ("img_general_meishi_base_story-bg.*", "base/commu", "16:9"),
-    ("img_general_meishi_base_(?!story-bg).*full\..*", "base/misc", "16:9"),
-    ("img_general_achievement_produce.*", "achievement/produce", None),
-    ("img_general_achievement_common.*", "achievement/misc", None),
-    ("img_general_meishi_illust_music-logo.*", "parts/logo", "3:2"),
-    ("img_general_meishi_illust_pict-icon.*", "parts/icon", None),
-    ("img_general_meishi_illust_stamp.*", "parts/misc", None),
-    ("img_general_meishi_illust_hatsuboshi-logo.*", "parts/misc", None),
-    ("img_general_meishi_illust_event.*", "parts/event", None),  # Inferred
-    ("img_general_meishi_illust_highscore.*", "parts/highscore", None),  # Inferred
-    ("img_general_skillcard.*", "produce/skillcard", None),
-    ("img_general_pitem.*", "produce/pitem", None),
-    ("img_general_pdrink.*", "produce/pdrink", None),
+    (r"img_general_icon_contest-rank.*", "profile", None),
+    (r"img_general_meishi_illust_idol.*", "idol/full", None),
+    (r"img_general_meishi_illust_sd.*", "idol/mini", "2:3"),
+    (r"img_general_cidol.*full\..*", "idol/produce", "9:16"),
+    (r"img_general_cidol.*thumb-landscape-large.*", "idol/produce", "16:9"),
+    (r"img_general_cidol.*thumb-portrait.*", "idol/produce", "3:4"),
+    (r"img_general_meishi_illust_sign.*", "idol/sign", None),
+    (r"img_general_csprt.*full\..*", "support", "16:9"),
+    (r"img_general_meishi_base_story-bg.*", "base/commu", "16:9"),
+    (r"img_general_meishi_base_(?!story-bg).*full\..*", "base/misc", "16:9"),
+    (r"img_general_achievement_produce.*", "achievement/produce", None),
+    (r"img_general_achievement_common.*", "achievement/misc", None),
+    (r"img_general_meishi_illust_music-logo.*", "parts/logo", "3:2"),
+    (r"img_general_meishi_illust_pict-icon.*", "parts/icon", None),
+    (r"img_general_meishi_illust_stamp.*", "parts/misc", None),
+    (r"img_general_meishi_illust_hatsuboshi-logo.*", "parts/misc", None),
+    (r"img_general_meishi_illust_event.*", "parts/event", None),  # Inferred
+    (r"img_general_meishi_illust_highscore.*full\..*", "parts/highscore", None),  # Inferred
+    (r"img_general_skillcard.*", "produce/skillcard", None),
+    (r"img_general_pitem.*", "produce/pitem", None),
+    (r"img_general_pdrink.*", "produce/pdrink", None),
 ]
 
 # Have to hardcode the number 10, otherwise this requires empty folder post-detection
@@ -59,6 +59,7 @@ if __name__ == "__main__":
     target = f"namecard_kit_v{manifest.revision}/"  # output directory
 
     for pattern, subdir, ratio in instructions_dl:
+        logger.info(f"Populating '{subdir}'")
         manifest.download(
             pattern,
             path=target + subdir,
